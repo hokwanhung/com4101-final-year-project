ae341e081afa2ffa385d0b0b716fb492eb907ca1
khho <hokwanhung.2019@gmail.com>
2023-12-26 06:02:23 +0800

    fix: fix payment_card_number values regarding security reasons

--- sql/users.sql
+++ sql/users.sql
@@ -13,12 +13,11 @@ CREATE TABLE users (
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255),
   phone_number VARCHAR(20) NOT NULL,
-  password VARCHAR(255) NOT NULL,
   birth_date DATE NOT NULL,
   identity_no VARCHAR(20) NOT NULL,
   loyalty_points INT(11) DEFAULT 0,
   payment_method VARCHAR(255),
-  payment_card_number VARCHAR(20),
+  payment_card_number_final_four_digit VARCHAR(20),
   payment_card_expiry VARCHAR(10),
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
