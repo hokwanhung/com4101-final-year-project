1022d692ca22f05eab4a3218cc1977b47d4be38f
khho <hokwanhung.2019@gmail.com>
2023-12-25 18:39:37 +0800

    feat: implement special_view column in hotel_rooms.sql

--- sql/hotel_rooms.sql
+++ sql/hotel_rooms.sql
@@ -3,10 +3,13 @@
 -- TIMESTAMP is used to record any changes for the record.
 -- ON UPDATE CURRENT_TIMESTAMP means the column value will be updated automatically when a record is updated.
 
+-- special_view expects values such as NULL, "Mountain", and "Harbour", in which the prices gradually increases by HKD 100.
+
 CREATE TABLE hotel_rooms (
   id BINARY(16) NOT NULL PRIMARY KEY,
   room_number INT(11) AUTO_INCREMENT NOT NULL,
   room_type VARCHAR(255) NOT NULL,
+  special_view VARCHAR(255) DEFAULT NULL,
   room_capacity INT(11) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
   availability BOOLEAN NOT NULL DEFAULT 1,
